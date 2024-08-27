80268b7d87d532fbb2998eb7558de2d704d2eead
Debiprasad Mohanty <145470177+Debiprasadmohanty18@users.noreply.github.com>
2024-08-27 22:56:02 +0530

    Update FetchingAllDataFromDatabase.java

--- FetchingAllDataFromDatabase.java
+++ FetchingAllDataFromDatabase.java
@@ -1,21 +1,21 @@
 
-import java.sql.*;  // import java.sql package
+import java.sql.*;  // import java.sql package.
 
 public class FetchingAllDataFromDatabase {
 
 	public static void main(String[] args) {  // We can use try{} and catch{} block or we can use throws keyword directly in main method.
 		try {
 		Class.forName("com.mysql.jdbc.Driver"); // Load & Register the Driver
-		Connection built = DriverManager.getConnection("jdbc:mysql://localhost:3306/student","root","Debi@prasad9777");  // Connection Established
-		Statement think = built.createStatement();  // Create Statement
-		ResultSet store = think.executeQuery("select * from studentdata");  // Execute Query
+		Connection built = DriverManager.getConnection("jdbc:mysql://localhost:3306/student","root","Debi@prasad9777");  // Connection Established.
+		Statement think = built.createStatement();  // Create Statement.
+		ResultSet store = think.executeQuery("select * from studentdata");  // Execute Query.
 		
 		while(store.next()) 
 		{
 			String show = store.getInt(1) + " : " + store.getString(2) + " : " + store.getString(3);   // Process Results
-				  		// AS id number is an integer so getInt(ColumnNumber) required
-						// As name is a String type so getString(ColumnNumber) required
-						// As address is also String type so getString(ColumnNumber) required
+				  		// AS id number is an integer so getInt(ColumnNumber) required.
+						// As name is a String type so getString(ColumnNumber) required.
+						// As address is also String type so getString(ColumnNumber) required.
 			System.out.println(show); 
 		}
 		
